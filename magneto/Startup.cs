9de84906d85b58a581ec681cdb81ebefafe86a3a
JUNAID <JUNAID@DESKTOP-H8A0N31>
2018-02-02 21:45:49 +0530

    Add project files.

---
+++ magneto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(magneto.Startup))]
+namespace magneto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
